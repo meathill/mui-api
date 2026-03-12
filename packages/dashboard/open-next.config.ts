f56b33aee6aef6c5ed4afb4cffed6d92bfcbe54b
Meathill <meathill@gmail.com>
2026-03-12 17:49:33 +0800

    change: fixed wrangler.jsonc

--- packages/dashboard/open-next.config.ts
+++ packages/dashboard/open-next.config.ts
@@ -1,9 +1,11 @@
 import { defineCloudflareConfig } from '@opennextjs/cloudflare';
+import r2IncrementalCache from '@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache';
+import doQueue from '@opennextjs/cloudflare/overrides/queue/do-queue';
+import d1TagCache from '@opennextjs/cloudflare/overrides/tag-cache/d1-next-tag-cache';
 
 export default defineCloudflareConfig({
-  // Uncomment to enable R2 cache,
-  // It should be imported as:
-  // `import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";`
-  // See https://opennext.js.org/cloudflare/caching for more details
-  // incrementalCache: r2IncrementalCache,
+  incrementalCache: r2IncrementalCache,
+  queue: doQueue,
+  tagCache: d1TagCache,
+  enableCacheInterception: true,
 });
